39ebb56e15911da86519e9684c89796791fe9ef0
Amartya <62711821+amartyaay@users.noreply.github.com>
2022-08-20 10:26:59 +0530

    added express,socket,mongoDb to server side

--- server/index.js
+++ server/index.js
@@ -1,1 +1,32 @@
-console.log("Hello from Server");
+console.log("Hello from Server.");
+const { Socket } = require("dgram");
+const express = require("express");
+const htttp = require("http");
+const mongoose = require("mongoose");
+const app = express();
+const port = process.env.port || 3000;
+
+var server = htttp.createServer(app);
+var io = require("socket.io")(server);
+
+//middle ware
+app.use(express.json());
+
+io.on("connection", (socket) => {
+  console.log("socket connected");
+});
+
+const DB =
+  "mongodb+srv://amartya:amartya@cluster0.eurd8ke.mongodb.net/?retryWrites=true&w=majority";
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("DB connected");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+server.listen(port, "0.0.0.0", () => {
+  console.log(`server started on  port ${port}`);
+});
